dd889680b7d9f800346e9cdb44ed1d25c961d342
Hilal Bulut <hilal.bulut@hs-furtwangen.de>
2020-05-31 14:21:07 +0200

    Aufgabe6

--- Aufgabe6/Aufgabe.6.js
+++ Aufgabe6/Aufgabe.6.js
@@ -3,7 +3,7 @@ var L06_Haushaltshilfe;
 (function (L06_Haushaltshilfe) {
     window.addEventListener("load", handleLoad);
     let form;
-    let url = "http://localhost:5001";
+    let url = "https://buluthil.herokuapp.com/";
     async function handleLoad(_event) {
         console.log("Init");
         let response = await fetch("Data.json");
